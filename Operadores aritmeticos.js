7ae4e0840519b76a6a7fa12e2549e2dd007d5342
Samuel24067 <solopez16@soy.sena.edu.co>
2023-02-22 13:58:38 -0500

    talleres de operadoreslogicos

---
+++ Operadores aritmeticos.js
@@ -0,0 +1,15 @@
+const prompt =require('prompt-sync')();
+let n1=prompt("Elija un numero: ");
+let n2=prompt("Elija otro numero: ");
+
+
+let suma= n1+n2;
+let multiplicacion= n1*n2;
+let resta=n1-n2;
+let division=n1/n2;
+
+console.log("Suma= "+suma);
+console.log("Resta= "+resta);
+console.log("Multiplicacion= "+multiplicacion);
+console.log("Division= "+division);
+
